13045df330b7813531931a8b98a9c933133bdd6e
YashMistry1406 <yashmistry2000@gmail.com>
2021-01-30 18:08:42 +0530

    dialog left

--- app/src/main/java/com/example/ad340/details/ForecastDetailsActivity.kt
+++ app/src/main/java/com/example/ad340/details/ForecastDetailsActivity.kt
@@ -1,9 +1,14 @@
 package com.example.ad340.details
+//package com.example.ad340.details
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.TextView
+import android.widget.Toast
 import com.example.ad340.R
 import kotlinx.android.synthetic.main.activity_main.*
 import org.w3c.dom.Text
@@ -11,6 +16,8 @@ import org.w3c.dom.Text
 class ForecastDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forecast_details)
+
 
         setTitle(R.string.forecast_details)
 
@@ -19,9 +26,8 @@ class ForecastDetailsActivity : AppCompatActivity() {
         val descriptionText=findViewById<TextView>(R.id.descriptionText)
         //val temp=intent.getFloatExtra("key_map",0f)
 
+
         val temp=intent.getFloatExtra("key_temp",0f)
-
-
 
 
 
@@ -29,4 +35,22 @@ class ForecastDetailsActivity : AppCompatActivity() {
         descriptionText.text=intent.getStringExtra("key_description")
 //
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater:MenuInflater=menuInflater
+        inflater.inflate(R.menu.settings_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.tempDisplaySetting-> {
+                Toast.makeText(this, "clicked emnu item",Toast.LENGTH_SHORT).show()
+                true
+
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
